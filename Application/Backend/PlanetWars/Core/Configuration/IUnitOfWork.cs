d6dddd23d84c7648e14bdc0657c8c76f230ab9d9
Nevena Tufegdzic <nevenatufegdzic9@gmail.com>
2022-01-08 19:29:40 +0100

    Update IUnitOfWork

--- Application/Backend/PlanetWars/Core/Configuration/IUnitOfWork.cs
+++ Application/Backend/PlanetWars/Core/Configuration/IUnitOfWork.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using PlanetWars.Core.IRepositories;
 
 namespace PlanetWars.Core.Configuration
 {
     public interface IUnitOfWork
     {
-        //TODO: Add IRepositories
-        
+        IUserRepository Users { get; }
+        IPlayerRepository Players { get; }
+        IPlayerColorRepository PlayerColors { get; }
+        IColorRepository Colors { get; }
+        IPlanetRepository Planets { get; }
+        IGalaxyRepository Galaxies { get; }
+        ISessionRepository Sessions { get; }
         Task CompleteAsync();
     }
 }
