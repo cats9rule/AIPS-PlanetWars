2494a9391d14e57e6ab378ae68b94cbf2d5bbfb7
welcomepeople <djoksy99@gmail.com>
2022-01-08 22:12:56 +0100

    planetDto i hope, i promena u sessionrepository

---
+++ Application/Backend/PlanetWars/DTOs/LowGravityPlanetDto.cs
@@ -0,0 +1,8 @@
+
+namespace PlanetWars.DTOs
+{
+    public class LowGravityPlanetDto : PlanetDto
+    {
+        public int MovementBoost { get; set; }
+    }
+}
---
+++ Application/Backend/PlanetWars/DTOs/PlanetDto.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace PlanetWars.DTOs
+{
+    public class PlanetDto
+    {
+        public int ID { get; set; }
+        public string Owner { get; set; }
+        public int ArmyCount { get; set; }
+        public List<int> NeighbourPlanets {get; set; }
+    }
+}
